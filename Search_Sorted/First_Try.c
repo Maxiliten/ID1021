b59ae097189587ff16ae6adbc2c523ea047faf92
Maxiliten <daderman.maximilian@gmail.com>
2025-01-27 13:04:33 +0100

    test

--- Search_Sorted/First_Try.c
+++ Search_Sorted/First_Try.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <string.h>
 #include <time.h>
+#include <windows.h>
 
 /*Unsorted search, [SKELETTKOD]*/
 bool unsorted_search(int array[], int length, int key)
@@ -61,28 +62,41 @@ int *Random_Array_Generator(int length, int element_max_value)
 int main()
 {
     // Configuration menu
-    int array_length = 10000;
-    int array_element_size = 10;
-    int key_value = 9;
+    int array_length = 1000000;
+    int array_element_size = 1000000;
+    int key_value = rand()%100000;
+    double interval = 0;
+    double interval2 = 0;
 
-    int *sorted_array = sorted(array_length);
+    for (int i = 0; i < 1000; i++){
     int *random_array = Random_Array_Generator(array_length, array_element_size);
+     LARGE_INTEGER frequency; 
+     LARGE_INTEGER start; 
+     LARGE_INTEGER end; 
+     QueryPerformanceFrequency(&frequency); 
+     QueryPerformanceCounter(&start);
+     unsorted_search(random_array, array_length, key_value);
+      // code to be measured 
+     QueryPerformanceCounter(&end); 
+     interval += (double) (end.QuadPart - start.QuadPart) / frequency.QuadPart;
+    }
+    double average_unsorted_time = interval/1000;
+    printf("Average unsorted time search: %lf\n", average_unsorted_time);
 
-    // Unsorted search
-    clock_t time_unsorted_start = clock();
-    unsorted_search(random_array, array_length, key_value);
-    clock_t time_unsorted_end = clock();
-    double total_time_unsorted = (double)(time_unsorted_end - time_unsorted_start) / CLOCKS_PER_SEC;
-    printf("Time for unsorted search:%lf\n", total_time_unsorted);
-
-    // Sorted search (optimised)
-    clock_t time_sorted_start = clock();
+    for (int i = 0; i < 1000; i++){
+    int *sorted_array = sorted(array_length);
+     LARGE_INTEGER frequency2; 
+     LARGE_INTEGER start2; 
+     LARGE_INTEGER end2; 
+     QueryPerformanceFrequency(&frequency2); 
+     QueryPerformanceCounter(&start2);
     sorted_search(sorted_array, array_length, key_value);
-    clock_t time_sorted_end = clock();
-    double total_time_sorted = (double)(time_sorted_end - time_sorted_start) / CLOCKS_PER_SEC;
-    printf("Time for sorted search:%lf\n", total_time_sorted);
+      // code to be measured 
+     QueryPerformanceCounter(&end2); 
+     interval2 += (double) (end2.QuadPart - start2.QuadPart) / frequency2.QuadPart; 
+    }
+    double average_sorted_time = interval2/1000;
+    printf("Average sorted search: %lf", average_sorted_time);
 
-    double time_difference = total_time_unsorted - total_time_sorted;
-    printf("Time Comparison, sorted search is:%lf", time_difference, "faster");
 }
-/*Oavsett hur stor lista jag gör tar själva sökningen under 1 sekund? Även om jag sätter det sökta elementet sist medvetet? Annars 0 sekunder */
+/*Oavsett hur stor lista jag gör tar själva sökningen under 1 sekund? Även om jag sätter det sökta elementet sist medvetet? Annars 0 sekunder  */
